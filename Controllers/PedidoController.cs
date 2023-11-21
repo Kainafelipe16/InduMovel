8b842572a21d9aa72ac43497cc8f962a219189a4
Kainã Felipe de Oliveira Coelho <kaina.coelho@portalsesi.org.br>
2023-11-21 10:58:07 -0300

    commit 2111

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -22,7 +22,19 @@ namespace InduMovel.Controllers
         }
         public IActionResult Checkout()
         {
-            return View();
+            UserAcount userlogado = _context.Users.FirstOrDefault(p => p.UserName == User.Identity.Name);
+            Pedido p = new Pedido {
+                Email = userlogado.Email,
+                Nome = userlogado.Nome,
+                Endereco1 = userlogado.Endereco,
+                Numero = userlogado.Numero,
+                Bairro = userlogado.Bairro,
+                Cidade = userlogado.Cidade,
+                Estado = userlogado.Estado,
+                Cep = userlogado.Cep.ToString()
+            };
+
+            return View(p);
         }
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
@@ -56,6 +68,7 @@ namespace InduMovel.Controllers
                 ViewBag.TotalPedido = _carrinho.GetCarrinhoCompraTotal();
                 //limpa o carrinho do cliente
                 _carrinho.LimparCarrinho();
+                ViewBag.UrlWhatsApp = EnviarMensagem(pedido.PedidoId);
                 //exibe a view com dados do cliente e do pedido
                 return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
             }
@@ -64,6 +77,28 @@ namespace InduMovel.Controllers
             public async Task<IActionResult> List(){
                 var appDbContext = _context.Pedidos;
                 return View(await appDbContext.ToListAsync());
+            }
+            public string EnviarMensagem(int pedidoId)
+            {
+                var pedido = _context.Pedidos.Include(p => p.PedidoMoveis).ThenInclude(p => p.Movel).FirstOrDefault(p => p.PedidoId == pedidoId);
+
+                string numeroDestinatario = "+5518997923191";
+
+                string mensagem = "Pedido: " + pedido.PedidoId.ToString() + "Cliente: "+pedido.Nome.ToString();
+
+                // Formate o número removendo caracteres não numéricos
+                    string numeroFormatado = new
+                    
+                    string(numeroDestinatario.Where(char.IsDigit).ToArray());
+
+                    // Construa o link do WhatsApp
+                    string urlWhatsApp =
+
+                    $"https://web.whatsapp.com/send?phone=+{numeroFormatado}&text={Uri.EscapeDataString(mensagem)}";
+
+                    Console.WriteLine(urlWhatsApp);
+                    // Retorne a View que contém o script JavaScript
+                    return urlWhatsApp;
             }
     }
 }
